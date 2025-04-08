3df1eacb21ecde9072c9b10815a2edfc9a50d5a0
Justin Baldeosingh <justin.baldeosingh@teleios-systems.com>
2025-04-08 16:00:22 -0400

    doc: Added documentation page.

--- VideoPlatform.Web/Controllers/HomeController.cs
+++ VideoPlatform.Web/Controllers/HomeController.cs
@@ -33,6 +33,12 @@ namespace VideoPlatform.Web.Controllers {
         }
 
         [AllowAnonymous]
+        public IActionResult Documentation()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error() {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
